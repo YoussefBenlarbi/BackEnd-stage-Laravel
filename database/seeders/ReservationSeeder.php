d403e788b03ba5c8947c168d86e582340802fd95
Youssef <benlarbi1youssef@gmail.com>
2023-05-18 12:44:41 +0100

    stable version 18/0(/2023

--- database/seeders/ReservationSeeder.php
+++ database/seeders/ReservationSeeder.php
@@ -17,27 +17,27 @@ class ReservationSeeder extends Seeder
     {
 
 
-        Reservation::create([
-            'user_id'   => 3,
-            'car_id' => 27,
-            'date_start' => '2023-05-23',
-            'date_end' => '2023-05-29',
-            'note' => 'note merigla',
-        ]);
-        Reservation::create([
-            'user_id'   => 4,
-            'car_id' => 28,
-            'date_start' => '2023-5-01',
-            'date_end' => '2023-05-19',
-            'note' => 'note merigla',
-        ]);
-        Reservation::create([
-            'user_id'   => 5,
-            'car_id' => 29,
-            'date_start' => '2023-5-12',
-            'date_end' => '2023-6-20',
-            'note' => 'note merigla',
-        ]);
-        // Reservation::factory(20)->create();
+        // Reservation::create([
+        //     'user_id'   => 3,
+        //     'car_id' => 27,
+        //     'date_start' => '2023-05-23',
+        //     'date_end' => '2023-05-29',
+        //     'note' => 'note merigla',
+        // ]);
+        // Reservation::create([
+        //     'user_id'   => 4,
+        //     'car_id' => 28,
+        //     'date_start' => '2023-5-01',
+        //     'date_end' => '2023-05-19',
+        //     'note' => 'note merigla',
+        // ]);
+        // Reservation::create([
+        //     'user_id'   => 5,
+        //     'car_id' => 29,
+        //     'date_start' => '2023-5-12',
+        //     'date_end' => '2023-6-20',
+        //     'note' => 'note merigla',
+        // ]);
+        Reservation::factory(20)->create();
     }
 }
